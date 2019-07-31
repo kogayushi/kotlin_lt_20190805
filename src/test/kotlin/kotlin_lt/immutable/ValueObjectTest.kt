9b417aef2ce2b2d573fbcad278d10115081960e3
yushi koga <youshe.old.pleasure@gmail.com>
2019-08-01 08:52:42 +0900

    update

--- src/test/kotlin/kotlin_lt/immutable/ValueObjectTest.kt
+++ src/test/kotlin/kotlin_lt/immutable/ValueObjectTest.kt
@@ -4,31 +4,15 @@ import org.hamcrest.core.IsEqual.equalTo
 import org.hamcrest.core.IsNot.not
 import org.junit.Assert.assertThat
 import org.junit.Test
-import kotlin.test.assertTrue
 
 
 internal class ValueObjectTest {
     @Test
-    fun testEqualsToStringHashCode() {
-        // set up
-        val valueObject = ValueObject("value object", "other value")
-        val otherValueObject = ValueObject("value object", "other value")
-
-        // exercise
-        val actual = valueObject == otherValueObject // これはequalsの呼び出し
-
-        // verify
-        assertTrue(actual) // equalsがオーバーライドされてる
-        // toString()がオーバーライドされてる
-        assertThat(valueObject.toString(), equalTo(otherValueObject.toString()))
-        // hashCode()がオーバーライドされてる
-        assertThat(valueObject.hashCode(), equalTo(otherValueObject.hashCode()))
-    }
-
-    @Test
     fun testCopy() {
         // set up
-        val valueObject = ValueObject("value object", "other value")
+        val valueObject = ValueObject(mainValue = "value object", otherValue = "other value")
+
+        // exercise
         val otherValueObject = valueObject.copy(otherValue = "copied object")
 
         // verify
